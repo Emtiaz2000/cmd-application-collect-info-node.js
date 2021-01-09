3ffde36dd848149c68521f90d127ec5196b6e3cd
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2021-01-09 20:01:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const program = require('commander');
 const validetor = require('validator')
 const chalk = require('chalk')
 const inquire = require('inquirer')
-const {addContract,checkMail,listContracts} = require('./contact')
+const {addContract,checkMail,listContracts,deleteContract,findContract} = require('./contact')
 
 const addQuestons = [
     {
@@ -52,17 +52,21 @@ const addQuestons = [
         choices:['presonal','professional']
         
     }
-]
+    
 
+]
 
 program.version('0.0.1')
 program.description('A Command Line Application')
 
+
+
+
+
 program
     .command("add")
     .alias('a')
-    .description('please add a contract')
-    // .requiredOption('-f,--firstName <fname>', 'Type your first name')
+    .description('please add a contract')  // .requiredOption('-f,--firstName <fname>', 'Type your first name')
     // .requiredOption('-l,--lastName <lname>', 'Type your last name')
     // .requiredOption('-e,--email <email>', 'Type your email')
     // .option('-t,--type <contract>', 'Type your contract', 'personal')
@@ -74,6 +78,8 @@ program
     })
 
 
+    
+
 
 //geting  all contract 
 program.command('list')
@@ -82,9 +88,74 @@ program.command('list')
     await listContracts()
 })
 
+
+
+const deleteQuestion = [{
+    type:'input',
+    name:'email',
+    massage:'enter your email',
+    validate(input){
+        if(!input || !validetor.isEmail(input)){
+            console.log(chalk.red.inverse('please type valide emali'))
+        }else{
+            return true;
+        }
+    }
+}]
+
+
+
+// deleting contract
+program.command('delete')
+.alias('del')
+.description('Enter email to delete contracts')
+// .requiredOption('-e,--email <email>',"type your email")
+.action( async ()=>{
+    //console.log(email)
+    const res = await inquire.prompt(deleteQuestion)
+    //console.log(res.email)
+     deleteContract(res.email)
+})
+
+
+
+
+//find any contract 
+
+
+const findingContract = [{
+    type:'input',
+    name:'email',
+    massage:"Type your email",
+    validate(input){
+        if(!input || !validetor.isEmail(input)){
+            console.log(chalk.red.inverse('please type valide emali'))
+        }else{
+            return true;
+        }
+    }
+}]
+
+program.command('find')
+.alias('f')
+.description('Find your contract.')
+.action(async ()=>{
+    const result = await inquire.prompt(findingContract)
+    findContract(result.email)
+})
+
+
+
+
+
+
 if (!process.argv[2]) {
     program.help();
 }
 
 program.parse(process.argv)
+
+
+
+
 
